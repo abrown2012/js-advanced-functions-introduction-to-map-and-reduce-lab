32560d21b12320a3fc2e1e140c0dd10c761fbbf9
Agnieszka Brown <abrown2012@icloud.com>
2021-05-21 17:55:50 -0400

    commit tree db50af11d22bb993261794b38900c5c9f4d6a066

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+const mapToNegativize = (sourceArray) => {
+    return sourceArray.map(x => x * (-1))
+}
+
+const mapToNoChange = (sourceArray) => {
+    return sourceArray.map(x => x )
+}
+
+const mapToDouble = (sourceArray) => {
+    return sourceArray.map(x => x * 2 )
+}
+
+const mapToSquare = (sourceArray) => {
+    return sourceArray.map(x => x*x)
+}
+
+const reduceToTotal = (sourceArray, startingPoint=0) => {
+    return startingPoint + sourceArray.reduce((total, currentValue) => total + currentValue)
+}
+
+const reduceToAllTrue = (sourceArray) => {
+    return sourceArray.reduce(
+        (total, amount)  => { if (!!total == true && !!amount == true)
+    {return true} else {
+      return false}
+    }
+    )
+}
+
+const reduceToAnyTrue = (sourceArray) => {
+    return sourceArray.reduce(
+        (total, amount)  => { if (total == true)
+    {return true} else {
+      return !!amount}
+    }
+    )
+}
